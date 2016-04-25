008334bdead57a6768a49baf0f41be1a70f3ba3a
iainscott-rgu <odyssey3007@hotmail.com>
2016-04-25 10:21:04 +0100

    yea

--- addbugs.php
+++ addbugs.php
@@ -7,18 +7,7 @@
     <link rel="stylesheet" type="text/css" href="assets/CSS/unsemantic-grid-responsive-tablet.css">
     <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 
-    <?php
-    include("connect.php");
-    $name = $_POST["name"];
-    $summary = $_POST["summary"];
-    $category = $_POST["category"];
-    $sql = "INSERT INTO bugs (name, summary, category) VALUES ($name','$summary','$category')";
-    if(mysqli_query($db, $sql)) {
-    }else{
-        echo "Error: " . $sql . "<br>" . mysqli_error($db);
-    }
-    header("location:index.php");
-    ?>
+
 
 </head>
 <body>
@@ -79,7 +68,18 @@
 
 </main>
 
+<?php
+include("connect.php");
+$name = $_POST["name"];
+$summary = $_POST["summary"];
+$category = $_POST["category"];
+$sql = "INSERT INTO bugs (name, summary, category) VALUES ($name','$summary','$category')";
+if(mysqli_query($db, $sql)) {
+}else{
+    echo "Error: " . $sql . "<br>" . mysqli_error($db);
+}
 
+?>
 <footer>
     <p>Designed by Iain Scott, 2016.</p>
 
