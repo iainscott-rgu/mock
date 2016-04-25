e6f352ccba005a40df92fd811009bf24b59cb955
iainscott-rgu <odyssey3007@hotmail.com>
2016-04-25 14:10:20 +0100

    sdgciasegdfiashegfiaw

--- showbugs.php
+++ showbugs.php
@@ -56,9 +56,9 @@
 
 
         <table class='display'>
-            <tr><td><input type='text' name='link' size='100' value='{$name}'></td></tr>
-            <tr><td><textarea cols='101' rows='5' name='summary'>'{$summary}'</textarea></td></tr>
-            <tr><td><input type='text' name='category' size='100' value='{$category}'></td></tr>
+            <tr><td><input type='text' name='link' size='100' value='{$name}' readonly></td></tr>
+            <tr><td><textarea cols='101' rows='5' name='summary' readonly>'{$summary}'</textarea></td></tr>
+            <tr><td><input type='text' name='category' size='100' value='{$category}' readonly></td></tr>
         </table>
     </form>
     <hr>";
