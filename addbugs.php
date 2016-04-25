e2bfde72269de120412a9b694749cbaf97a46b56
iainscott-rgu <odyssey3007@hotmail.com>
2016-04-25 10:24:37 +0100

    yea

--- addbugs.php
+++ addbugs.php
@@ -37,7 +37,7 @@
 
     <section class="grid-66">
 
-        <form>
+        <form action="insert.php" method="post">
             <table>
                 <tr><td>
                         <label for="name">Bug Name:</label>
@@ -68,18 +68,7 @@
 
 </main>
 
-<?php
-include("connect.php");
-$name = $_POST["name"];
-$summary = $_POST["summary"];
-$category = $_POST["category"];
-$sql = "INSERT INTO bugs (name, summary, category) VALUES ($name','$summary','$category')";
-if(mysqli_query($db, $sql)) {
-}else{
-    echo "Error: " . $sql . "<br>" . mysqli_error($db);
-}
 
-?>
 <footer>
     <p>Designed by Iain Scott, 2016.</p>
 
---
+++ insert.php
@@ -0,0 +1,12 @@
+<?php
+include("connect.php");
+$name = $_POST["name"];
+$summary = $_POST["summary"];
+$category = $_POST["category"];
+$sql = "INSERT INTO bugs (name, summary, category) VALUES ($name','$summary','$category')";
+if(mysqli_query($db, $sql)) {
+}else{
+    echo "Error: " . $sql . "<br>" . mysqli_error($db);
+}
+header("location:index.php");
+?>
