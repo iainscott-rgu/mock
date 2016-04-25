5abf0a0f0667c2f792ed4a8a2a3c0662746e28ab
iainscott-rgu <odyssey3007@hotmail.com>
2016-04-25 10:40:54 +0100

    yea

--- insert.php
+++ insert.php
@@ -3,7 +3,7 @@ include("connect.php");
 $name = $_POST["name"];
 $summary = $_POST["summary"];
 $category = $_POST["category"];
-$sql = "INSERT INTO bugs (name, summary, category) VALUES ($name','$summary','$category')";
+$sql = "INSERT INTO bugs (name, summary, category) VALUES ('$name','$summary','$category')";
 if(mysqli_query($db, $sql)) {
 }else{
     echo "Error: " . $sql . "<br>" . mysqli_error($db);
